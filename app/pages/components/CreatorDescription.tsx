857be2e7a7209e67feeaced42043fa22d5801fbd
migui3230 <migui0323@gmail.com>
2022-03-10 16:06:01 -0500

    added creatorLandingPage.tsx file and dummy CreatorDescription.tsx file

---
+++ app/pages/components/CreatorDescription.tsx
@@ -0,0 +1,19 @@
+
+
+
+const CreatorDescription = () => {
+  return (
+    <div className="">
+      <p className="text-base font-light leading-relaxed mt-0 mb-4 text-black">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Pellentesque maximus, ipsum eu dignissim consectetur, nisi nunc
+        efficitur nunc, eget efficitur nunc nisi eu nunc.
+      </p>
+    </div>
+  );
+};
+
+export default CreatorDescription;
+
+
+

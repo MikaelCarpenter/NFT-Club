40ab2956dc28ef2e1c67381dbc343d3e24547372
migui3230 <migui0323@gmail.com>
2022-03-10 15:50:00 -0500

    created name header component for title of creator page

---
+++ app/pages/components/NameHeader.tsx
@@ -0,0 +1,13 @@
+
+const NameHeader = () => {
+    return (
+        <div className="text-center">
+            <h1 className="font-medium leading-tight text-4xl mt-0 mb-2 text-black">NAME</h1>
+        </div>
+    );
+};
+
+export default NameHeader;
+
+
+

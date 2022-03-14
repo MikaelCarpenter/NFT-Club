a05dd883d6adf4d03b2e87e46b3dab0ea7472736
migui3230 <migui0323@gmail.com>
2022-03-14 13:20:14 -0400

    modified benefit box and creator description components to look like figma component

---
+++ app/pages/components/BenefitBox.tsx
@@ -0,0 +1,12 @@
+
+const BenefitBox = () => {
+  return (
+    <div className="box-border my-4 h-28 w-3/5 p-4  border-2 border-black">
+    </div>
+  );
+};
+
+export default BenefitBox;
+
+
+
--- app/pages/components/CreatorDescription.tsx
+++ app/pages/components/CreatorDescription.tsx
@@ -3,8 +3,8 @@
 
 const CreatorDescription = () => {
   return (
-    <div className="">
-      <p className="text-base font-light leading-relaxed mt-0 mb-4 text-black">
+    <div className="w-96">
+      <p className="text-base font-light leading-relaxed mt-0 mb-4 text-center text-black">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Pellentesque maximus, ipsum eu dignissim consectetur, nisi nunc
         efficitur nunc, eget efficitur nunc nisi eu nunc.

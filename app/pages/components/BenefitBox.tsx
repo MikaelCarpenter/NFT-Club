f6197f0e01e48395a50c82649e1a5c4943717dfb
migui3230 <migui0323@gmail.com>
2022-03-14 20:20:37 -0400

    added more stylying and content to the benefit boxes

--- app/pages/components/BenefitBox.tsx
+++ app/pages/components/BenefitBox.tsx
@@ -1,7 +1,18 @@
 
 const BenefitBox = () => {
   return (
-    <div className="box-border my-4 h-28 w-3/5 p-4  border-2 border-black">
+    <div className="box-border my-4 h-28 w-3/5 p-2  border-2 border-black">
+      <div>
+        <p className= "text-black top-0 left-0 font-medium">
+          Benefit
+        </p>
+      </div>
+      <div className="flex flex col items-center justify-center">
+        <p className="text-black text-center">
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quos quaerat, doloremque,
+        </p>
+      </div>
+
     </div>
   );
 };

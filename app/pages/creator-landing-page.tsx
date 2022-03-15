df3eb625cd7497b2571568cefaecd2656fbc3476
migui3230 <migui0323@gmail.com>
2022-03-14 20:39:34 -0400

    used tailwind `prose` class for the creator description component

--- app/pages/creator-landing-page.tsx
+++ app/pages/creator-landing-page.tsx
@@ -22,11 +22,14 @@ const BenefitBox = () => {
 const CreatorDescription = () => {
   return (
     <div className="w-96">
-      <p className="text-base font-light leading-relaxed mt-0 mb-4 text-center text-black">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Pellentesque maximus, ipsum eu dignissim consectetur, nisi nunc
-        efficitur nunc, eget efficitur nunc nisi eu nunc.
-      </p>
+      <article className="prose-sm">
+        <p className="font-light text-center text-black">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Pellentesque maximus, ipsum eu dignissim consectetur, nisi nunc
+          efficitur nunc, eget efficitur nunc nisi eu nunc.
+        </p>
+      </article>
+
     </div>
   );
 };

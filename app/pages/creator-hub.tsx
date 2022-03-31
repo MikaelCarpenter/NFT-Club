1fd86e73ed4a34e30ac3083811728fcbf657deb4
Kitan Garcia <kitangar@gmail.com>
2022-03-31 16:40:23 -0700

    implement updating Creators on frontend

--- app/pages/creator-hub.tsx
+++ app/pages/creator-hub.tsx
@@ -25,6 +25,16 @@ const connection = new anchor.web3.Connection(
 const CreatorHub = () => {
   const { user } = useUser();
   const [benefits, setBenefits] = useState<Array<Benefit>>([]);
+  const [creatorUsername, setCreatorUsername] = useState(
+    user.creatorAccount.username
+  );
+  const [creatorEmail, setCreatorEmail] = useState(user.creatorAccount.email);
+  const [creatorDescription, setCreatorDescription] = useState(
+    user.creatorAccount.description
+  );
+  const [editName, setEditName] = useState(false);
+  const [editEmail, setEditEmail] = useState(false);
+  const [editDescription, setEditDescription] = useState(false);
 
   const connectedWallet = useAnchorWallet();
   const program = useMemo(() => {
@@ -48,21 +58,6 @@ const CreatorHub = () => {
       );
 
       console.log(user.creatorAccount);
-      /*
-      console.log('=============================');
-      const beans = await program.account.benefit.all([
-        {
-          memcmp: {
-            offset: 8, //Discriminator
-            bytes: connectedWallet.publicKey.toBase58(),
-          },
-        },
-      ]);
-      console.log(beans);
-      console.log(beans[0].publicKey.toBase58());
-      console.log(beans[0].account);
-      console.log('=============================');
-      */
 
       const benefitArray = [];
       for (let i = 1; i <= user.creatorAccount.numBenefits; i++) {
@@ -142,6 +137,7 @@ const CreatorHub = () => {
         if (newBenefit) {
           benefitArray.push(newBenefit as Benefit);
           setBenefits(benefitArray);
+          // Should re-render with new Benefits...
         }
       } catch (error) {
         console.log(error);
@@ -155,20 +151,161 @@ const CreatorHub = () => {
     }
   }, [connectedWallet, program, user, getBenefits]);
 
+  const editCreator = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    type: string
+  ) => {
+    if (type === 'username') {
+      setCreatorUsername(event.target.value);
+    }
+    if (type === 'description') {
+      setCreatorDescription(event.target.value);
+    }
+    if (type === 'email') {
+      setCreatorEmail(event.target.value);
+    }
+  };
+
+  const updateAccount = async () => {
+    if (creatorUsername.length === 0 || creatorDescription.length === 0) {
+      alert('A creator must have a username or description');
+      return;
+    }
+    if (creatorUsername.length > 42 || creatorDescription.length > 420) {
+      alert(
+        'Creator username must be <= 42 chars and description must be <= 420 chars'
+      );
+      return;
+    }
+
+    if (
+      creatorUsername === user.creatorAccount.username &&
+      creatorEmail === user.creatorAccount.email &&
+      creatorDescription === user.creatorAccount.description
+    ) {
+      return;
+    }
+
+    if (program && connectedWallet) {
+      const creatorSeeds = [
+        connectedWallet.publicKey.toBuffer(),
+        Buffer.from('creator'),
+      ];
+
+      const [creatorPubKey] = await anchor.web3.PublicKey.findProgramAddress(
+        creatorSeeds,
+        program.programId
+      );
+
+      try {
+        await program.rpc.updateAccount(
+          creatorUsername,
+          creatorEmail,
+          creatorDescription,
+          {
+            accounts: {
+              creator: creatorPubKey,
+              authority: program.provider.wallet.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+            },
+          }
+        );
+
+        const updatedAccount = await program.account.creator.fetch(
+          creatorPubKey
+        );
+        if (updatedAccount) {
+          console.log('UPDATE USER');
+          console.log(updatedAccount);
+          // update User
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const toggleEditCreator = (type: string) => {
+    if (type === 'username') {
+      setEditName(!editName);
+    }
+    if (type === 'email') {
+      setEditEmail(!editEmail);
+    }
+    if (type === 'description') {
+      setEditDescription(!editDescription);
+    }
+  };
+
   // Do all of this if a creator is found
   // All creator fields are mandatory, so extra checking should not be necessary?: Check this...
   return (
     <div className="flex h-full w-full flex-col items-center">
       {user.creatorAccount && (
         <div className="prose mb-8 w-3/5 text-center">
-          <h2>{user.creatorAccount.username}</h2>
+          <div>
+            {!editName ? (
+              <h2 className="inline">{user.creatorAccount.username}</h2>
+            ) : (
+              <input
+                className="input-value ml-2 rounded-xl bg-slate-200 p-1 text-primary"
+                value={creatorUsername}
+                onChange={(e) => editCreator(e, 'username')}
+              ></input>
+            )}
+            <p
+              className="inline cursor-pointer pl-2 text-gray-400"
+              onClick={() => toggleEditCreator('username')}
+            >
+              {!editName ? 'Edit' : 'Cancel'}
+            </p>
+            <div>
+              {!editEmail ? (
+                <p className="inline">{user.creatorAccount.email}</p>
+              ) : (
+                <input
+                  className="input-value ml-2 rounded-xl bg-slate-200 p-1 text-primary"
+                  value={creatorEmail}
+                  onChange={(e) => editCreator(e, 'email')}
+                ></input>
+              )}
+              <p
+                className="inline cursor-pointer pl-2 text-gray-400"
+                onClick={() => toggleEditCreator('email')}
+              >
+                {!editEmail ? 'Edit' : 'Cancel'}
+              </p>
+            </div>
+          </div>
           <div className="flex justify-around">
             <p>Revenue: $80000</p>
             {user.subscriptions && (
               <p>Subscribers: {user.subscriptions.length}</p>
             )}
           </div>
-          <p>{user.creatorAccount.description}</p>
+          <div>
+            {!editDescription ? (
+              <p className="inline">{user.creatorAccount.description}</p>
+            ) : (
+              <input
+                className="input-value ml-2 rounded-xl bg-slate-200 p-1 text-primary"
+                value={creatorDescription}
+                onChange={(e) => editCreator(e, 'description')}
+              ></input>
+            )}
+            <p
+              className="inline cursor-pointer pl-2 text-gray-400"
+              onClick={() => toggleEditCreator('description')}
+            >
+              {!editDescription ? 'Edit' : 'Cancel'}
+            </p>
+          </div>
+          <button
+            className="btn btn-outline btn-sm mt-2 h-12 w-32"
+            onClick={updateAccount}
+          >
+            Update Account
+          </button>
         </div>
       )}
       {benefits && benefits.length > 0 && (
